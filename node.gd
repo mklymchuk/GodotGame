fc21b3a824d5b7d9c304e77a3503b430656b8505
mklymchuk <mklymchuk1994@gmail.com>
2023-08-17 19:43:40 +0300

    minor code changes

--- node.gd
+++ node.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+var a  = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_numbers()
@@ -12,33 +12,19 @@ func _process(delta):
 	pass
 
 func _numbers():
+	#Method 1
+	var score: int = 0
+	var remaining_fuel: float = 99.9
+	var paused: bool = false
+	var player_name: String = ""
 	
-	#Integers
-	var a = 1
-	var b = -23
-	var c = 0
-	var d = 0xA1Fe316 #hexadecimal
-	var dd = 0xA1FE316 #hexadecimal
+	print(score, " ", remaining_fuel, " ", paused, " ", player_name, " ")
+	print(a)
 	
-	print(a, " ", b, " ", c, " ", d, " ", dd)
+	#Method 2 (inferring the type)
+	var my_int := 19
+	var size := 19.9
+	var running := true
+	var name := ""
 	
-	#Floats
-	var x = 1.0
-	var y = -43.01
-	var z = 1.3e6
-	var zz = 3.3e6
-	
-	print(x, " ", y, " ", z, " ", zz)
-	
-	#Constant
-	const THE_ANSWER = 42
-	
-	print(THE_ANSWER)
-	
-	#Predefined
-	print(PI)
-	print(-PI)
-	print(TAU)
-	print(INF)
-	print(-INF)
-	
+	print(my_int, " ", size, " ", running, " ", name)
